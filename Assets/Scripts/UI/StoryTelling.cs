f56ec8615c6009b0ca58dfce22c94f79c176b4c8
Beliz Yazıcı <beliz.yazici@std.izmirekonomi.edu.tr>
2024-06-07 01:24:36 +0300

    script conflict problem is solved

--- Assets/Scripts/UI/StoryTelling.cs
+++ Assets/Scripts/UI/StoryTelling.cs
@@ -7,13 +7,10 @@ public class StoryTelling : MonoBehaviour
 {
     [SerializeField] private GameObject talk1;
     [SerializeField] private GameObject talk2;
-    [SerializeField] private float talk1Duration = 2f; 
+    [SerializeField] private float talk1Duration = 2f;
     [SerializeField] private float talk2Duration = 2f; 
     [SerializeField] private MonoBehaviour characterMovementScript; 
-    [SerializeField] private GameObject ammo;
     [SerializeField] private GameObject crosshair;
-    [SerializeField] private GameObject motherImage;
-    [SerializeField] private GameObject characterImage;
 
     public GameObject storyPanel;
 
@@ -27,22 +24,19 @@ public class StoryTelling : MonoBehaviour
         if (characterMovementScript != null)
         {
             characterMovementScript.enabled = false;
-            ammo.SetActive(false);
         }
 
         ActivateText1();
         yield return new WaitForSeconds(talk1Duration);
 
         ActivateText2();
-        ActivateMotherImage();
         yield return new WaitForSeconds(talk2Duration);
-        
+
         CloseStoryPanel();
 
         if (characterMovementScript != null)
         {
             characterMovementScript.enabled = true;
-            ammo.SetActive(true);
             crosshair.SetActive(true);
         }
     }
@@ -55,13 +49,6 @@ public class StoryTelling : MonoBehaviour
     public void ActivateText2()
     {
         talk1.SetActive(false);
-        talk2.SetActive(true);
-    }
-
-    public void ActivateMotherImage()
-    {
-        characterImage.SetActive(false);
-        motherImage.SetActive(true);
         talk2.SetActive(true);
     }
 
---
+++ Assets/Scripts/UI/StoryTelling2.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StoryTelling2 : MonoBehaviour
+{
+    [SerializeField] private GameObject talk1;
+    [SerializeField] private GameObject talk2;
+    [SerializeField] private float talk1Duration = 2f; 
+    [SerializeField] private float talk2Duration = 2f; 
+    [SerializeField] private MonoBehaviour characterMovementScript; 
+    [SerializeField] private GameObject ammo;
+    [SerializeField] private GameObject crosshair;
+    [SerializeField] private GameObject motherImage;
+    [SerializeField] private GameObject characterImage;
+
+    public GameObject storyPanel;
+
+    void Start()
+    {
+        StartCoroutine(ShowStorySequence());
+    }
+
+    private IEnumerator ShowStorySequence()
+    {
+        if (characterMovementScript != null)
+        {
+            characterMovementScript.enabled = false;
+            ammo.SetActive(false);
+        }
+
+        ActivateText1();
+        yield return new WaitForSeconds(talk1Duration);
+
+        ActivateText2();
+        ActivateMotherImage();
+        yield return new WaitForSeconds(talk2Duration);
+        
+        CloseStoryPanel();
+
+        if (characterMovementScript != null)
+        {
+            characterMovementScript.enabled = true;
+            ammo.SetActive(true);
+            crosshair.SetActive(true);
+        }
+    }
+
+    public void ActivateText1()
+    {
+        talk1.SetActive(true);
+    }
+
+    public void ActivateText2()
+    {
+        talk1.SetActive(false);
+        talk2.SetActive(true);
+    }
+
+    public void ActivateMotherImage()
+    {
+        characterImage.SetActive(false);
+        motherImage.SetActive(true);
+        talk2.SetActive(true);
+    }
+
+    public void CloseStoryPanel()
+    {
+        storyPanel.SetActive(false);
+    }
+}
